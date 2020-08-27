79f9c65589d17a55972e70f6bf39f47ad6a7a9dd
NAO <zackzakumi62@outlook.jp>
2020-08-27 14:09:42 +0000

    top page

---
+++ resources/views/commons/navbar.blade.php
@@ -0,0 +1,20 @@
+<header class="mb-4">
+   <nav class="navbar navbar-expand-sm navbar-gray bg-pink">
+       {{-- トップページへのリンク --}}
+       <a class="navbar-brand" href="/">ちっご巡り</a>
+       
+        <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#nav-bar">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        
+        <div class="collapse navbar-collapse" id="navbar">
+            <ul class="navbar-nav mr-auto"></ul>
+            <ul class="navbar-nav">
+                {{-- ユーザー登録ページへのリンク　--}}
+                <li class="nav-item"><a href="#"　class="nav-link">新規登録</a></li>
+                {{-- ログインページへのリンク　--}}
+                <li class="nav-item"><a href="#"　class="nav-link">ログイン</a></li>
+            </ul>
+        </div>
+　　</nav> 
+</header>
